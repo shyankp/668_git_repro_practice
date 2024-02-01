13f81d15dbe202c208d4687d5d472da2247cad47
shyankp <shyankp25@gmail.com>
2024-02-01 10:24:59 -0800

    Added ‘this is silly’ to cat_names.py

--- cat_names.py
+++ cat_names.py
@@ -1,4 +1,5 @@
-print("My cat's names are Fiona and Ella. Fiona is doesn't like the roomba, so we named the roomba Farquad")
+print("My cat's names are Fiona and Ella. Fiona doesn't like the roomba, so we named the roomba Farquad")
 
 
 #Below, write a print statement that will print "That's silly" when this code is run
+print("That's silly")
